ff8088b690e1b7b799d7258d998a05ef4da5c29b
Hồ Bá Thanh <157885927+thanhhb1@users.noreply.github.com>
2025-05-19 00:13:43 +0700

    xong hideUser (#15)

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,15 +1,17 @@
 import express from "express";
 import {
     getUsers,
-    create
+    create,
+    hide,
+    unHide
 } from "../controllers/user.js";
 
 const routerUser=express.Router();
 
 routerUser.get("/",getUsers);
 routerUser.post("/",create);
-
-
+routerUser.patch("/:id/hide", hide);
+routerUser.patch("/:id/unhide", unHide);
 
 
 export default routerUser;
