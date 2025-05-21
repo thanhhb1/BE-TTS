8cb93ff9a133029f54a9270214576a74f46b633b
HO Thanh <thanhhbph50161@gmail.com>
2025-05-21 14:54:42 +0700

    sua lai user

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import routerCategory from './category.js'; 
-import routerUser from './user.js'; 
-import routerRole from './role.js'; 
+import routerUser from './user.js';  
 import routerProduct from './product.js';
 // import routerAuth from './auth.js';
 
@@ -10,7 +9,6 @@ const router = express.Router();
 
 router.use('/categories', routerCategory);
 router.use('/users', routerUser);
-router.use('/roles', routerRole);
 router.use('/products', routerProduct);
 
 // import routerAuth from './auth.js';
--- src/validation/user.js
+++ src/validation/user.js
@@ -1,21 +1,20 @@
 
 import Joi from "joi";
-import mongoose from "mongoose";
+
 
 export const userValid = Joi.object({
   fullname: Joi.string().min(3).max(255).required(),
-  email: Joi.string().email({ tlds: { allow: false } }).required().messages({
+  email: Joi.string().email().required().messages({
     'string.email': 'Email không hợp lệ',
   }),
   password: Joi.string().min(6).required(),
   phone: Joi.string().pattern(/^[0-9]{10,15}$/).messages({
     'string.pattern.base': 'Số điện thoại không hợp lệ',
   }),
-  role_id: Joi.string().custom((value, helpers) => {
-    if (!mongoose.Types.ObjectId.isValid(value)) {
-      return helpers.message("role_id không hợp lệ");
-    }
-    return value;
-  }),
+  role: Joi.string().valid('admin', 'user', 'manage').required(),
   status: Joi.boolean(),
 });
+
+export const roleValid = Joi.object({
+  role: Joi.string().valid('admin', 'user', 'manage').required()
+}).unknown(true);
