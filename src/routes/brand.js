bfd2cb92cba81505a9a52be8a7b115527f193a37
HO Thanh <thanhhbph50161@gmail.com>
2025-05-28 20:54:53 +0700

    permission

--- src/routes/brand.js
+++ src/routes/brand.js
@@ -8,15 +8,15 @@ import {
   restoreBrand,
   forceDeleteBrand
 } from '../controllers/brand.js';
-
+import { authenticate, authorizeRoles } from "../middlewares/auth.js";
 const routerBrand = express.Router();
 
-routerBrand.get('/', getBrands); 
-routerBrand.get('/trash', getDeletedBrands); 
-routerBrand.post('/', createBrand);
-routerBrand.put('/:id', updateBrand);
-routerBrand.delete('/:id', removeBrand);
-routerBrand.patch('/restore/:id', restoreBrand);
-routerBrand.delete('/forcedelete/:id', forceDeleteBrand); 
+routerBrand.get('/',authenticate, authorizeRoles("admin", "manage"), getBrands); 
+routerBrand.get('/trash',authenticate, authorizeRoles("admin"), getDeletedBrands); 
+routerBrand.post('/',authenticate, authorizeRoles("admin", "manage"), createBrand);
+routerBrand.put('/:id',authenticate, authorizeRoles("admin", "manage"), updateBrand);
+routerBrand.delete('/:id',authenticate, authorizeRoles("admin"), removeBrand);
+routerBrand.patch('/restore/:id',authenticate, authorizeRoles("admin"), restoreBrand);
+routerBrand.delete('/forcedelete/:id',authenticate, authorizeRoles("admin"), forceDeleteBrand); 
 
 export default routerBrand;
